379ca4b02804d71b1f5385d4e4858b380c46b1a1
Tien <tientaitu98@gmail.com>
2019-04-08 08:42:28 +0700

    thu link https://www.google.com/ thi duoc, nhung https://google.com/ thi khong duoc

--- CropLapp/app/src/main/java/com/example/croplapp/WebViewActivity.java
+++ CropLapp/app/src/main/java/com/example/croplapp/WebViewActivity.java
@@ -1,8 +1,10 @@
 package com.example.croplapp;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class WebViewActivity extends AppCompatActivity {
 
@@ -11,9 +13,17 @@ public class WebViewActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.view_web);
 
-        String url = "https://croplab.vn/blog";
-        WebView view= (WebView)  this.findViewById(R.id.webView);
-        view.getSettings().setJavaScriptEnabled(true);
-        view.loadUrl(url);
+//        String url = "https://croplab.vn/";
+        String url = "https://croplab.vn/";
+        WebView webView= (WebView)  findViewById(R.id.webView);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setVerticalScrollBarEnabled(false);
+        webView.setHorizontalScrollBarEnabled(false);
+        webView.getSettings().setLoadWithOverviewMode(true);
+        webView.getSettings().setUseWideViewPort(true);
+        webView.getSettings().setBuiltInZoomControls(true);
+        webView.getSettings().setDisplayZoomControls(false);
+        webView.getSettings().setDomStorageEnabled(true);
+        webView.loadUrl("https://www.google.com/");
     }
 }
