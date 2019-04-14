62dab91e33778678d63f9335f82fdc9472a0b882
Tien Nguyen <tientaitu98@gmail.com>
2019-04-14 11:31:57 +0700

    Code convention
    thêm ghi chú vào code

--- CropLapp/app/src/main/java/com/example/croplapp/MainActivity.java
+++ CropLapp/app/src/main/java/com/example/croplapp/MainActivity.java
@@ -1,3 +1,11 @@
+/*
+* MainActivity.java
+*
+* Nguyen Duc Tien 16020175
+* 
+* Home screen
+*/
+
 package com.example.croplapp;
 
 import android.content.Context;
@@ -18,88 +26,114 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    /* String specifying the search area: "Hà Nội" or "Sài Gòn" */
     String loadArea;
+    
+    /* Request code for startActivityForResult() & onActivityResult() function */
     private static final int REQUEST_CODE = 1998;
+    
+    /* Internet connection test function */
     private boolean isNetworkConnected() {
         ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
-
         return cm.getActiveNetworkInfo() != null;
     }
-
+    
+    /* Alert dialog */
     public void showAlertDialog(final int response, String code) {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setTitle("CropLab Thông báo!!!");
         switch (response) {
             case 4:
             {
+                // Set the message
                 builder.setMessage("Bạn đang ngoại tuyến!!! \nKết nối mạng và thử lại");
             }
         }
-
+        /* Disable click outside the alert to turn off */
         builder.setCancelable(false);
+        /* Set "OK" button */
         builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialogInterface, int i) {
+                // Dismiss the alert
                 dialogInterface.dismiss();
+                // Exit when there is no internet connection
                 finish();
             }
         });
-//
+        /* Creat alert on buffer */
         AlertDialog alertDialog = builder.create();
+        /* Show alert dialog */
         alertDialog.show();
     }
-
+    
+    /* Get saved data from android-shared-preferences */
     private void loadAppSetting()  {
         SharedPreferences sharedPreferences= this.getSharedPreferences("croplabSetting", Context.MODE_PRIVATE);
         if(sharedPreferences!= null) {
+            // String specifying the search area
             loadArea = sharedPreferences.getString("area", getString(R.string.areaHanoi));
         }
     }
-
+    
+    /* Save data to android-shared-preferences */
     public void saveAppSetting()  {
-        // File chia sẻ sử dụng trong nội bộ ứng dụng, hoặc các ứng dụng được chia sẻ cùng User.
         SharedPreferences sharedPreferences= this.getSharedPreferences("croplabSetting", Context.MODE_PRIVATE);
-
         SharedPreferences.Editor editor = sharedPreferences.edit();
         editor.putString("area", loadArea);
-        // Save.
+        // Save
         editor.apply();
     }
+    
+    /* onCreat(); */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        //check internet
+        
+        /* Check internet */
         if (isNetworkConnected()) {
             Log.e("print", "You're connected to us");
-            //load saved area
+            // Load saved data
             loadAppSetting();
         } else{
             Log.e("print", "You're alone");
+            // Show alert offline and exit
             showAlertDialog(4,"4");
         }
-
-
     }
+    
+    /* onstart(); */
     @Override
     protected void onStart() {
         super.onStart();
         setContentView(R.layout.activity_main);
 
+        /* Show name of search area */
         TextView showArea = findViewById(R.id.textView);
         showArea.setText(loadArea);
-
+        
+        /*
+        * #1
+        * This function will coming soon
+        */
+        /* Action switch to FilmStoreActivity.java */
         Button buttonfilmstore = findViewById(R.id.button_film_store);
         buttonfilmstore.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Log.d("FilmstoreActivity", "onClick: ");
+                
                 Intent intent = new Intent(MainActivity.this, FilmStoreActivity.class);
+                // Start activity
                 startActivity(intent);
             }
         });
-
+        /*
+        * #1
+        */
+        
+        /* Action switch to TrackingActivity.java */
         Button buttontracking = findViewById(R.id.button_tracking);
         buttontracking.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -107,14 +141,17 @@ public class MainActivity extends AppCompatActivity {
                 Log.d("TrackingActivity", "onClick: ");
 
                 Intent intent1 = new Intent(MainActivity.this, TrackingActivity.class);
+                // Send bundle data (seach area) to destination activity
                 Bundle bundle = new Bundle();
                 bundle.putString("area",loadArea);
                 intent1.putExtras(bundle);
+                // Start activity
                 startActivity(intent1);
 
             }
         });
-
+        
+        /* Action switch to WebViewActivity.java */
         Button buttonnews = findViewById(R.id.button_news);
         buttonnews.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -125,53 +162,66 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        /* Action switch to OptionList.java */
         ImageButton buttonoption = findViewById(R.id.button_option);
         buttonoption.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Log.d("OptionList", "onClick: ");
                 Intent intent3 = new Intent(MainActivity.this, OptionList.class);
+                
+                // Send bundle data (seach area) to destination activity
                 Bundle bundle = new Bundle();
                 bundle.putString("area",loadArea);
                 intent3.putExtras(bundle);
-//                startActivity(intent3);
+                
+                // Start activity and get result when called activity return
                 startActivityForResult(intent3, REQUEST_CODE);
             }
         });
     }
-
+    
+    /* onActivityResult(); */
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        /* Get data return */
         super.onActivityResult(requestCode, resultCode, data);
-
-        // Kiểm tra requestCode có trùng với REQUEST_CODE vừa dùng
+        /* Check if the requestCode matches the REQUESTCODE we just used (REQUEST_CODE = 1998) */
         if(requestCode == REQUEST_CODE) {
-
-            // resultCode được set bởi DetailActivity
-            // RESULT_OK chỉ ra rằng kết quả này đã thành công
+            /*
+            * resultCode set by DetailActivity
+            * RESULT_OK indicates that this result was successful
+            */
             if(resultCode == AppCompatActivity.RESULT_OK) {
-                // Nhận dữ liệu từ Intent trả về
+                // Receive data from returned Intent
                 loadArea = data.getStringExtra(OptionList.EXTRA_DATA);
-
             } else {
-                // DetailActivity không thành công, không có data trả về.
+                // DetailActivity failed, no data returned.
             }
         }
     }
 
-    private static final int TIME_INTERVAL = 2000; // # milliseconds, desired time passed between two back presses.
+    /*
+    * onbackpressed();
+    * Press back 2 times to exit and save data to android-shared-preferences
+    */
+    
+    private static final int TIME_INTERVAL = 2000;  // milliseconds, desired time passed between two back presses.
     private long mBackPressed;
     @Override
     public void onBackPressed()
     {
+        // press back again in the given time period (<2s)
         if (mBackPressed + TIME_INTERVAL > System.currentTimeMillis())
         {
             super.onBackPressed();
+            // save data
             saveAppSetting();
             return;
+        } else {
+            // Press back first time
+            Toast.makeText(getBaseContext(), "Nhấn phím back lần nữa để thoát", Toast.LENGTH_SHORT).show();
         }
-        else { Toast.makeText(getBaseContext(), "Nhấn phím back lần nữa để thoát", Toast.LENGTH_SHORT).show(); }
-
         mBackPressed = System.currentTimeMillis();
     }
 }
