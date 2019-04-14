e26d1ae5065f968b0052ed8b4e090d450e400e4b
Tien Nguyen <tientaitu98@gmail.com>
2019-04-14 17:35:28 +0700

    Update InformationActivity.java
    Thêm chú thích

--- CropLapp/app/src/main/java/com/example/croplapp/InformationActivity.java
+++ CropLapp/app/src/main/java/com/example/croplapp/InformationActivity.java
@@ -1,17 +1,18 @@
-package com.example.croplapp;
+/*
+* Name: InformationActivity.java
+* Author: Nguyen Duc Tien 16020175
+* Purpose: Infor screen
+*/
 
+package com.example.croplapp;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 public class InformationActivity extends AppCompatActivity {
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.info_layout);
-
     }
-
 }
