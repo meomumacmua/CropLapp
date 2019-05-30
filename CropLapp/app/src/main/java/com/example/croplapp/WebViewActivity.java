172c4e370e0c9f8130bdde321fb4617dfbdecc9d
Tien Nguyen <tientaitu98@gmail.com>
2019-05-30 12:17:48 +0700

    Update WebViewActivity.java

--- CropLapp/app/src/main/java/com/example/croplapp/WebViewActivity.java
+++ CropLapp/app/src/main/java/com/example/croplapp/WebViewActivity.java
@@ -1,10 +1,14 @@
+/*
+ * OMane: WebViewActivity.java
+ * Author: Duong Quoc Anh 16020102
+ * Purpose: open webview, access Croplab fanpage
+ * Include: ProgressBar, WebView
+ */
 package com.example.croplapp;
 
 import android.annotation.SuppressLint;
 import android.graphics.Bitmap;
 import android.os.Bundle;
-
-
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
